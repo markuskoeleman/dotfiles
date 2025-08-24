7ee64e1c5875be2eeaa21989220e314c1419adca
Markus <markus.koeleman@gmail.com>
2025-08-24 02:02:13 +0200

    changed some nnvim config things

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -711,7 +711,7 @@ require("lazy").setup({
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				-- clangd = {},
+				clangd = {},
 				-- gopls = {},
 				-- pyright = {},
 				-- rust_analyzer = {},
@@ -773,6 +773,9 @@ require("lazy").setup({
 					end,
 				},
 			})
+
+			vim.lsp.enable("rust_analyzer")
+			vim.lsp.enable("zls")
 		end,
 	},
 
