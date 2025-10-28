103db5f20e7f1ad8b7c5cdd0056deb080015fa39
Markus <markus.koeleman@gmail.com>
2025-10-28 12:31:06 +0100

    added open_github.sh

---
+++ .local/bin/open_github.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+BROWSER="firefox"
+
+url=$(git remote get-url origin) 
+
+if [[ $url == *github.com* ]]; then
+    if [[ $url == git@* ]]; then
+        url="${url#git@}"
+        url="${url/:/\/}" 
+        url="https://$url"
+    fi
+	$BROWSER "$url"
+else
+    echo "This repository is not hosted on GitHub"
+fi
--- .local/bin/open_pdf.sh
+++ .local/bin/open_pdf.sh
@@ -3,7 +3,11 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find $dir ~/uni/books ~/Downloads -mindepth 1 -maxdepth 1 -name "*.pdf" | fzf)
+    selected=$(find $dir ~/uni/books ~/Downloads -mindepth 1 -maxdepth 1 -name "*.pdf" | sed "s|^$HOME/||" | fzf)
+	    # Add home path back
+    if [[ -n "$selected" ]]; then
+        selected="$HOME/$selected"
+    fi
 fi
 
 if [[ -z $selected ]]; then
